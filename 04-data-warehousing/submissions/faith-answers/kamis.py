1ca132f179628f25d79ee0ce359b5eb27f4f01ee
WambuiGichuru <gichuru.w.faith@gmail.com>
2024-08-03 10:43:57 +0300

    Datawarehousing Pipeline snowflake connection trial

--- 04-data-warehousing/submissions/faith-answers/kamis.py
+++ 04-data-warehousing/submissions/faith-answers/kamis.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 import logging
+import snowflake.connector
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s-%(levelname)s-%(message)s')
 
@@ -34,9 +35,67 @@ def clean_data(data):
         logging.error(f"Unsuccessful clean: {e}")
         raise
 
+#loading data inot snowflake
+def load_to_snowflake(data, conn_params, table_name):
+    try:
+        #connection
+        conn = snowflake.connector.connect(
+            user=conn_params['user'],
+            password=conn_params['password'],
+            account=conn_params['account'],
+            warehouse=conn_params['warehouse'],
+            database=conn_params['database'],
+            schema=conn_params['schema']
+        )
+        cur=conn.cursor()
+        # Create a table if not exists
+        create_table_query =""" 
+        CREATE TABLE IF NOT EXISTS AGRIC_COMMODITIES (
+            id INT,
+            commodity STRING,
+            grade STRING,
+            sex STRING,
+            market STRING,
+            wholesale FLOAT,
+            retail FLOAT,
+            supply_volume FLOAT,
+            county STRING,
+            date DATE,
+            PRIMARY KEY (id)
+        );
+        """
+        cur.execute(create_table_query)
+
+        # Insert data into the table
+        for index, row in data.iterrows():
+            insert_query = f"""
+            INSERT INTO AGRIC_COMMODITIES (id, commodity, grade, sex, market, wholesale, retail, supply_volume, county, date) VALUES
+            ({row['id']}, '{row['commodity']}', '{row['grade']}', '{row['sex']}', '{row['market']}', {row['wholesale']}, {row['retail']}, {row['supply_volume']}, '{row['county']}', '{row['date']}');
+            """
+            cur.execute(insert_query)
+
+        conn.commit()
+        cur.close()
+        conn.close()
+        logging.info("Data loaded to Snowflake successfully")
+    except Exception as e:
+        logging.error(f"Error loading data to Snowflake: {e}")
+        raise
+
 #the main program
 def main():
     file_path = "D:/pythonProject/Data Engineering Workshop/q2-data-engineering-workshop/04-data-warehousing/data/sample_agric_commodities.csv"
+    
+    conn_params = {
+        'user': 'Gichuru',
+        'password': 'Kidsnextdoor01',
+        'account': 'RGZGUTT.KA63834',
+        'warehouse': 'COMPUTE_WH',
+        'database': 'ETLTUTORIALS',
+        'schema': 'PUBLIC'
+    }
+    table_name = 'AGRIC_COMMODITIES'
+
     data = load_data(file_path)
     cleaned_data = clean_data(data)
     cleaned_data.to_csv("D:/pythonProject/Data Engineering Workshop/q2-data-engineering-workshop/04-data-warehousing/data/cleaned_data.csv", index=False)
